61f104bd1e632f797bafb555e9e22c4ffcab237a
anchon1o <anchonio@gmail.com>
2025-07-09 19:02:11 +0200

    Create usuarios.js

---
+++ usuarios.js
@@ -0,0 +1,7 @@
+const usuarios = {
+  A000: { nombre: "Alumno A", rol: "alumno", nivel: "GE", curso: 2 },
+  A001: { nombre: "Alumno B", rol: "alumno", nivel: "GP", curso: 5 },
+  P00:  { nombre: "Profesor 1", rol: "profesor" },
+  P01:  { nombre: "Profesor 2", rol: "profesor" },
+  admin: { nombre: "Administración", rol: "admin" }
+};
